d1f211b29af0359284c9d7b65a08b75f2ec0588a
Erika Contreras <ericontrerasleal2@gmail.com>
2023-03-10 14:30:56 -0500

    Add the rock paper scissors code to main.js file simplifying the function playRound using the conditional if and the logic operators

---
+++ main.js
@@ -0,0 +1,47 @@
+    const choices = ['rock', 'paper', 'scissors'];
+
+    function computerChoice () {
+           return choices[Math.floor(Math.random() * choices.length)];
+          }
+    function playerChoice () {
+        let input = prompt("Type rock, paper or scissors: ");
+          input = input.toLowerCase();
+         return input;
+         }
+
+        let playerSelection = playerChoice();
+        let computerSelection = computerChoice();
+        let playerScore = 0;
+        let computerScore = 0;
+
+        function playRound(playerSelection, computerSelection) {
+          if(playerSelection === computerSelection){
+            return "It is a tie. Try again";
+          } else if((playerSelection === "rock" && computerSelection == "scissors") || 
+          (playerSelection === "paper" && computerSelection == "rock") || 
+          (playerSelection === "scissors" && computerSelection == "paper")
+          ){
+            playerScore++;
+            return "Player Win: " + `${playerSelection}` + " beats " + `${computerSelection}`;
+          } else {
+            computerScore++;
+            return "Computer Win: " + `${computerSelection}` + " beats " + `${playerSelection}`;
+          }
+          }
+          
+          function game (playerSelection, computerSelection) {
+            for (let i= 0; i<5; i++) {
+              playerSelection = playerChoice();
+              computerSelection = computerChoice();
+              console.log(playRound(playerSelection, computerSelection));
+              console.log("Player Score: " + playerScore);
+              console.log("Computer Score: " + computerScore);
+              console.log("Computer chose: " + computerSelection);
+              console.log("Player chose: " + playerSelection);
+            }
+          }
+        
+        game ();
+
+        
+
